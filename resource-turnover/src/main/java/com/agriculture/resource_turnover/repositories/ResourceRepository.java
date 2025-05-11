7534a859643a21f4d9d5837fd0bc15059d16bc7d
maximzom <86885085+maximzom@users.noreply.github.com>
2025-05-11 22:16:16 +0300

    Delete resource-turnover directory

--- resource-turnover/src/main/java/com/agriculture/resource_turnover/repositories/ResourceRepository.java
+++
@@ -1,9 +0,0 @@
-package com.agriculture.resource_turnover.repositories;
-
-import com.agriculture.resource_turnover.models.Resource;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface ResourceRepository extends JpaRepository<Resource, Long> {
-    List<Resource> findByActive(boolean active);
-}
